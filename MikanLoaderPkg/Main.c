17e55d1c3a953670516ea4d2df729cafb1c6dd23
fnattou <guitarnattou.j@gmail.com>
2023-12-20 06:59:41 +0900

    first commit. for day02a

---
+++ MikanLoaderPkg/Main.c
@@ -0,0 +1,8 @@
+#include  <Uefi.h>
+#include  <Library/UefiLib.h>
+#include  <Library/UefiBootServicesTableLib.h>
+#include  <Library/PrintLib.h>
+#include  <Protocol/LoadedImage.h>
+#include  <Protocol/SimpleFileSystem.h>
+#include  <Protocol/DiskIo2.h>
+#include  <Protocol/BlockIo.h>
