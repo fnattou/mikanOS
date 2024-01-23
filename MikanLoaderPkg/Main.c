b3b18ce3782c494b112215609dbdb7afde53d564
fnattou <100653560+fnattou@users.noreply.github.com>
2024-01-24 07:11:44 +0900

    [day03b] 白い画面の描画

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -2,6 +2,7 @@
 #include  <Library/UefiLib.h>
 #include  <Library/UefiBootServicesTableLib.h>
 #include  <Library/PrintLib.h>
+#include  <Library/MemoryAllocationLib.h>
 #include  <Protocol/LoadedImage.h>
 #include  <Protocol/SimpleFileSystem.h>
 #include  <Protocol/DiskIo2.h>
@@ -109,6 +110,47 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
     return EFI_SUCCESS;
 }
 
+EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
+                   EFI_GRAPHICS_OUTPUT_PROTOCOL** gop) {
+  UINTN num_gop_handles = 0;
+  EFI_HANDLE* gop_handles = NULL;
+  gBS->LocateHandleBuffer(
+      ByProtocol,
+      &gEfiGraphicsOutputProtocolGuid,
+      NULL,
+      &num_gop_handles,
+      &gop_handles);
+
+  gBS->OpenProtocol(
+      gop_handles[0],
+      &gEfiGraphicsOutputProtocolGuid,
+      (VOID**)gop,
+      image_handle,
+      NULL,
+      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  FreePool(gop_handles);
+
+  return EFI_SUCCESS;
+}
+
+const CHAR16* GetPixelFormatUnicode(EFI_GRAPHICS_PIXEL_FORMAT fmt) {
+  switch (fmt) {
+    case PixelRedGreenBlueReserved8BitPerColor:
+      return L"PixelRedGreenBlueReserved8BitPerColor";
+    case PixelBlueGreenRedReserved8BitPerColor:
+      return L"PixelBlueGreenRedReserved8BitPerColor";
+    case PixelBitMask:
+      return L"PixelBitMask";
+    case PixelBltOnly:
+      return L"PixelBltOnly";
+    case PixelFormatMax:
+      return L"PixelFormatMax";
+    default:
+      return L"InvalidPixelFormat";
+  }
+}
+
 EFI_STATUS EFIAPI UefiMain(
   EFI_HANDLE image_handle,
   EFI_SYSTEM_TABLE* system_table) {
@@ -129,6 +171,28 @@ EFI_STATUS EFIAPI UefiMain(
 
   SaveMemoryMap(&memmap, memmap_file);
   memmap_file->Close(memmap_file);
+  //-------------------------------------
+  // draw pixel with gop
+  //-------------------------------------
+  EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
+  OpenGOP(image_handle, &gop);
+  EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE* m = gop->Mode;
+  Print(L"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n",
+    m->Info->HorizontalResolution,
+    m->Info->VerticalResolution, 
+    GetPixelFormatUnicode(m->Info->PixelFormat),
+    m->Info->PixelsPerScanLine
+  );
+  Print(L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n",
+    m->FrameBufferBase,
+    m->FrameBufferBase + m->FrameBufferSize,
+    m->FrameBufferSize
+  );
+
+  UINT8* frame_buffer = (UINT8*)m->FrameBufferBase;
+  for (UINTN i = 0; i < m->FrameBufferSize; ++i) {
+    frame_buffer[i] = 255;
+  }
 
   //-------------------------------------
   //read kernel
