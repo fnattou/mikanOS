7b632f26778d7105619091e01904ff5f7409ac7e
fnattou <100653560+fnattou@users.noreply.github.com>
2024-01-25 08:27:49 +0900

    [day03c] kernelからピクセルを描画。gitignoreにあとから記載したファイルを除外

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -228,12 +228,12 @@ EFI_STATUS EFIAPI UefiMain(
   if (EFI_ERROR(status)) {
     status = GetMemoryMap(&memmap);
     if (EFI_ERROR(status)) {
-      Print(L"failed to get memory map: &r\n", status);
+      Print(L"failed to get memory map: %r\n", status);
       while(1);
     }
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
     if (EFI_ERROR(status)) {
-      Print(L"Could not exit boot service: &r\n", status);
+      Print(L"Could not exit boot service: %r\n", status);
       while(1);
     }
   }
@@ -242,15 +242,12 @@ EFI_STATUS EFIAPI UefiMain(
   // Call Kernel
   //-------------------------------------
   UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
-  typedef void EntryPointType(void);
+  typedef void EntryPointType(UINT64, UINT64);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point();
+  entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
   
   Print(L"All done\n");
   
   while(1);
-
-
-  
   return EFI_SUCCESS;
 }
