151f397d46a71d0ef392e736c78ef2c4f7183c01
fnattou <100653560+fnattou@users.noreply.github.com>
2024-02-01 08:46:20 +0900

    [day05b] main.cpp分割

---
+++ kernel/.font.d
@@ -0,0 +1,5 @@
+font.o: font.cpp font.hpp \
+  /home/sota/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/sota/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/sota/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
---
+++ kernel/.graphics.d
@@ -0,0 +1,3 @@
+graphics.o: graphics.cpp graphics.hpp frame_buffer_config.hpp \
+  /home/sota/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/sota/osbook/devenv/x86_64-elf/include/c++/v1/__config
