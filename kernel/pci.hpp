bcec4c5fc30db12d690d45c85abd660f0314c505
fnattou <100653560+fnattou@users.noreply.github.com>
2024-02-05 07:52:22 +0900

    [day06c] usbマウスの認識とマウスの移動

--- kernel/pci.hpp
+++ kernel/pci.hpp
@@ -15,6 +15,30 @@ namespace pci {
     /// CONFIG_DATA レジスタのIOポートアドレス
     constexpr uint16_t kConfigData = 0xcfc;
 
+    /// @brief PCI デバイスのクラスコード
+    struct ClassCode {
+        uint8_t base, sub, interface;
+
+        /// @brief ベースクラスが等しい場合に真を返す 
+        bool Match(uint8_t b) {return b == base;}
+        /// @brief ベースクラス, サブクラスが等しい場合に真を返す 
+        bool Match(uint8_t b, uint8_t s) {return Match(b) && s == sub;}
+        /// @brief ベースクラス、サブクラス、インターフェイスが等しい場合に真を返す 
+        bool Match(uint8_t b, uint8_t s, uint8_t i) {
+            return Match(b, s) && i == interface;
+        }
+    };
+
+    /** PCI デバイスを操作するための基礎データを格納する
+     *
+     * バス番号，デバイス番号，ファンクション番号はデバイスを特定するのに必須．
+     * その他の情報は単に利便性のために加えてある．
+     * */
+    struct Device {
+        uint8_t bus, device, function, header_type;
+        ClassCode class_code;
+    };
+
     /// CONFIG_ADDRESS　に指定された整数を書き込む
     void WriteAddress(uint32_t address);
     /// CONFIG_DATA に指定された整数を書き込む
@@ -24,19 +48,21 @@ namespace pci {
 
     /// ベンダ ID レジスタを読み取る 
     uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function);
+    /// ベンダ ID レジスタを読み取る(デバイス指定)
+    inline uint16_t ReadVendorId(const Device& dev) {
+        return ReadVendorId(dev.bus, dev.device, dev.function);
+    }
     /// デバイス ID レジスタを読み取る 
     uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
-    /// ヘッダタイプレジスタを読み取る（全ヘッダタイプ共通） */
+    /// ヘッダタイプレジスタを読み取る（全ヘッダタイプ共通)
     uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
-    /** クラスコードレジスタを読み取る（全ヘッダタイプ共通）
-     *
-     * 返される 32 ビット整数の構造は次の通り．
-     *   - 31:24 : ベースクラス
-     *   - 23:16 : サブクラス
-     *   - 15:8  : インターフェース
-     *   - 7:0   : リビジョン
-     */
-    uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+    /// クラスコードレジスタを読み取る（全ヘッダタイプ共通）
+    ClassCode ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+    /// 
+    uint32_t ReadConfReg(const Device& dev, uint8_t reg_addr);
+
+    void WriteConfReg(const Device& dev, uint8_t reg_addr, uint32_t value);
+
     /** バス番号レジスタを読み取る（ヘッダタイプ 1 用）
      *
      * 返される 32 ビット整数の構造は次の通り．
@@ -49,15 +75,6 @@ namespace pci {
     /** 単一ファンクションの場合に真を返す． */
     bool IsSingleFunctionDevice(uint8_t header_type);
 
-    /** PCI デバイスを操作するための基礎データを格納する
-     *
-     * バス番号，デバイス番号，ファンクション番号はデバイスを特定するのに必須．
-     * その他の情報は単に利便性のために加えてある．
-     * */
-    struct Device {
-        uint8_t bus, device, function, header_type;
-    };
-
     /** ScanAllBus() により発見された PCI デバイスの一覧 */
     inline std::array<Device, 32> devices;
     /** devices の有効な要素の数 */
@@ -68,4 +85,9 @@ namespace pci {
      * 発見したデバイスの数を num_devices に設定する．
      */
     Error ScanAllBus();
+    constexpr uint8_t CalcBarAddress(unsigned int bar_index) {
+        return 0x10 + 4 * bar_index;
+    }
+
+    WithError<uint64_t> ReadBar(Device& device, unsigned int bar_index);
 }
