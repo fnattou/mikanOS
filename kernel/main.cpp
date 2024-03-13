ea37b153b09e2a0a2f0ebcd5d10b9f5dd4cd77f1
fnattou <100653560+fnattou@users.noreply.github.com>
2024-03-14 07:41:57 +0900

    [day10b] windowを表示

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -274,6 +274,13 @@ extern "C" void KernelMainNewStack(
     constexpr Vector2D<int> mouse_init_pos = {200, 200};
     mouse_position = mouse_init_pos;
 
+    auto main_window = std::make_shared<Window>(
+        160, 68, frame_buffer_config.pixel_format
+    );
+    DrawWindow(*main_window->Writer(), "Hello Window");
+    WriteString(*main_window->Writer(), {24, 28}, "Welcome to", {0, 0, 0});
+    WriteString(*main_window->Writer(), {24, 44}, "MikanOS world!", {0, 0, 0});
+
     FrameBuffer screen;
     if (const Error err = screen.Initialize(frame_buffer_config)) {
         Log(kError, "failed to initialize frame buffer: %s at %s:%d\n", 
@@ -291,9 +298,14 @@ extern "C" void KernelMainNewStack(
         .SetWindow(mouse_window)
         .Move(mouse_init_pos)
         .ID();
+    auto main_window_layer_id = layer_manager->NewLayer()
+        .SetWindow(main_window)
+        .Move({300, 300})
+        .ID();
     
     layer_manager->UpDown(bglayer_id, 0);
     layer_manager->UpDown(mouse_layer_id, 1);
+    layer_manager->UpDown(main_window_layer_id, 1);
     layer_manager->Draw();
 
 
