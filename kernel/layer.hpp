0ed7d1d1e7fa650d5be327ea4c6905efb00c8bd8
fnattou <100653560+fnattou@users.noreply.github.com>
2024-03-21 07:44:05 +0900

    [day10f] ウィンドウの移動を実装

--- kernel/layer.hpp
+++ kernel/layer.hpp
@@ -74,6 +74,8 @@ public:
     void UpDown(unsigned int id, int new_height);
     void Hide(unsigned int id);
 
+    /** @brief 指定された座標にウィンドウを持つ最も上に表示されているレイヤーを探す。 */
+    Layer* FindLayerByPosition(Vector2D<int> pos, unsigned int exclude_id) const;
 private:
     FrameBuffer* screen_{nullptr};
     mutable FrameBuffer back_buffer_{};
