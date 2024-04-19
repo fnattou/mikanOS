10638df54110d3d401e4b6e41a2d26fceef754a6
fnattou <100653560+fnattou@users.noreply.github.com>
2024-04-20 08:46:37 +0900

    [day21b] システムコール、printf実装

--- apps/rpn/rpn.cpp
+++ apps/rpn/rpn.cpp
@@ -1,6 +1,6 @@
 #include <cstring>
 #include <cstdlib>
-#include <cstdint>
+#include <cstdio>
 
 #include "../../kernel/logger.hpp"
 
@@ -18,8 +18,6 @@ void Push(long value) {
   stack[stack_ptr] = value;
 }
 
-extern "C" int64_t SyscallLogString(LogLevel, const char*);
-
 extern "C" int main(int argc, char** argv) {
   stack_ptr = -1;
   for (int i = 1; i < argc; ++i) {
@@ -27,23 +25,20 @@ extern "C" int main(int argc, char** argv) {
       long b = Pop();
       long a = Pop();
       Push(a + b);
-      SyscallLogString(kWarn, "+");
     } else if (strcmp(argv[i], "-") == 0) {
       long b = Pop();
       long a = Pop();
       Push(a - b);
-      SyscallLogString(kWarn, "-");
     } else {
       long a = atol(argv[i]);
       Push(a);
-      SyscallLogString(kWarn, "#");
     }
   }
   
-
-  if (stack_ptr < 0) {
-    return 0;
+  long result = 0;
+  if (stack_ptr >= 0) {
+    result = Pop();
   }
-  SyscallLogString(kWarn, "\nhello, this is rpn\n");
+  printf("%ld\n", result);
   while (1);
 }
